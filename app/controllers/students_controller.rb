ecd5702cedf5b01eaec856b22c548603e7b1dfb7
hopegipson <gipson.hope@gmail.com>
2020-10-22 00:05:11 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,23 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find_by(id: params[:id])
+    if @student.active == true 
+      @student.active = false
+      @student.save
+    else 
+      @student.active = true
+      @student.save
+     #student.update(active: true) other way to do it
+    end
+    redirect_to @student
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
